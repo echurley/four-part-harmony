b55baa4f61fb9b75132c7e53a42c7178749125e6
echurley <60479814+echurley@users.noreply.github.com>
2020-01-31 11:22:28 -0500

    Add files via upload
    Streamlined some things, added the ability to check if a voice in the its allowable range, made it so that the chord and the bassline are changed when the soprano voice is impossible to write. Sometimes the soprano voice adds a string instead of an integer. I think this comes from when the bass voice is generated, b/c sometimes it does weird stuff if I print out each bass note as they're written.

--- musicgenerator.py
+++ musicgenerator.py
@@ -1,4 +1,13 @@
+#c2 = 1
+#c3 = 8
+#c4 = 15
+#c5 = 22
+#an increase by one integer = movement up by one note name
+
+
+
 import random
+from inspect import signature
 #from midiutil import MIDIFile
 
 
@@ -14,14 +23,15 @@ import random
 
 chordList = ["1", "2", "3", "4", "5", "6", "7"]
 inversionList = ["  ", "6 ", "64"]
-chords = ["1"]
+chords = ["1  "]
 chordNotes = ["135", "246", "357", "461", "572", "613", "724"]
 beatNotes = ""
-b = [1]
-s = [17]
-a = []
-t = []
+b = ["b", 8]
+s = ["s", 22]
+a = ["a"]
+t = ["t"]
 parallelFifths = ""
+#ranges = [15, 26, 12, 22, 8, 19, 3, 15]
 
 
 
@@ -33,8 +43,8 @@ def inversionGenerator():
 #determine if two notes violate parallel fourths
 def parallelFourths(voice1, voice2):
     parallelFourths = ""
-    if (int(voice1[0]) - int(voice2[0])) % 8 == 5:
-        if (int(voice1[1]) - int(voice2[1])) % 8 == 5:
+    if (int(voice1[0]) - int(voice2[0])) % 7 == 5:
+        if (int(voice1[1]) - int(voice2[1])) % 7 == 5:
             parallelFourths = "y"
         else:
             parallelFourths = "n"
@@ -43,10 +53,11 @@ def parallelFourths(voice1, voice2):
     return(parallelFourths)
     
 #determine if two notes violate parallel octaves
-def parallelFifths(voice1, voice2):
+def parallelOctaves(voice1, voice2):
+    voice1 = voice1[len(voice1)-2:len(voice1)]
     parallelOctaves = ""
-    if (int(voice1[0]) - int(voice2[0])) % 8 == 5:
-        if (int(voice1[1]) - int(voice2[1])) % 8 == 5:
+    if (int(voice1[0]) - int(voice2[0])) % 7 == 5:
+        if (int(voice1[1]) - int(voice2[1])) % 7 == 5:
             parallelOctaves = "y"
         else:
             parallelOctaves = "n"
@@ -57,8 +68,11 @@ def parallelFifths(voice1, voice2):
 #determine if two notes violate parallel fifths
 def parallelFifths(voice1, voice2):
     parallelFifths = ""
-    if (int(voice1[0]) - int(voice2[0])) % 8 == 5:
-        if (int(voice1[1]) - int(voice2[1])) % 8 == 5:
+    voice1a = int(voice1[len(voice1) - 1])
+    voice2a = int(voice2[len(voice1) - 1])
+    print(voice1a, voice2a)
+    if (int(voice1[1]) - int(voice2[1])) % 7 == 5:
+        if (int(voice1[2]) - int(voice2[2])) % 7 == 5:
             parallelFifths = "y"
         else:
             parallelFifths = "n"
@@ -66,17 +80,47 @@ def parallelFifths(voice1, voice2):
         parallelFifths = "n"
     return(parallelFifths)
 
-#need to make sure voice is within the allowable range
+#make sure voice is within the allowable range
+def rangeFinder(voice):
+    length = len(voice) - 1
+    note = int(voice[length])
+    if voice[0] == "s":
+    	if note > 26 or note < 15:
+    	    inRange = "y"
+    	else:
+    	    inRange = "n"
+    elif voice[0] == "a":
+        if note > 22 or note < 12:
+            inRange = "y"
+        else:
+            inRange = "n"
+    elif voice[0] == "t":
+        if note > 19 or note < 8:
+            inRange = "y"
+        else:
+            inRange = "n"
+    else:
+        if note > 15 or note < 3:
+            inRange = "y"
+        else:
+            inRange = "n"
 
 #need to make sure the voices are close enough together
 
 #need to make sure there is no voice crossing
 
-#make is so the fifth isn't doubled
+#two leaps in a row
+
+#dont double leading tone (b)
+
+#try to weight nearest chord tones, yikes thats gonna suck
+
+#similar motion -> perfect w/o step in soprano
+
 
 
 #create random chord progression
-for beat in range(1,14):
+for beat in range(2,16):
     inversion = inversionGenerator()
     chord = random.choice(chordList)
     chord = chord + inversion
@@ -85,14 +129,15 @@ chords.append('5  ')
 chords.append('1  ')
 
 #determine what notes are available on each beat based on chord
-for beat in range(0, 16):
+for beat in range(1, 17):
     chord = chords[beat]
     root = chord[0]
     notes = chordNotes[int(root)-1]
     beatNotes = beatNotes + " " + notes
+#print(beatNotes)
 
 #create bass line from bass notes in chords
-for beat in range(1, 16):
+for beat in range(2, 17):
     chord = chords[beat]
     root = int(chord[0])
     inversion = chord[1:3]
@@ -103,29 +148,45 @@ for beat in range(1, 16):
     else:
         bNote = root + 4
     if bNote >= 8:
-        bNote = bNote - 8
+        bNote = bNote - 7
+    bNote = bNote + 7
     b.append(int(bNote))
 
-print(beatNotes)
 #create soprano line from available notes and rules about notes
-for beat in range(5, 64, 4):
-	notes = list(beatNotes[beat:beat + 3])
-	sNote = int(random.choice(notes)) + 16
-	s.append(sNote)
-	#determine if note breaks any rules
-	while parallelFifths(s[len(s)-2:len(s)],b[len(s)-2:len(s)]) == "y" or parallelFourths(s[len(s)-2:len(s)],b[len(s)-2:len(s)]) == "y" or parallelFourths(s[len(s)-2:len(s)],b[len(s)-2:len(s)]) == "y":
-		#if it does, delete it and try again, removing that note from the available ones
-		notes.remove(str(s[-1] - 16))
-		del s[-1]
-		sNote = random.choice(notes)
-		s.append(sNote)
-		#if there are no more available notes, change the chord, rinse and repeat
-		if len(notes) == 0:
-			chords[len(s)] = random.choice(chordList) + inversionGenerator()
-			break
-	#print(s)				
-	#print(s[len(s)-2:len(s)+1],b[len(s)-2:len(s)])
+for beat in range(1, 60, 4):
+    notes = list(beatNotes[beat:beat + 3])
+    #print(notes)
+    sNote = int(random.choice(notes)) + 14
+    s.append(sNote)
+    #determine if note breaks any rules
+    while parallelFifths(s, b) == "y" or parallelFourths(s[len(s)-2:len(s)], b[len(s)-2:len(s)]) == "y" or parallelFourths(s[len(s)-2:len(s)], b[len(s)-2:len(s)]) == "y" or rangeFinder(s):
+        #if it does, delete it and try again, remove note from available ones
+        notes.remove(str(int(s[-1]) - 14))
+        del s[-1]
+        sNote = random.choice(notes)
+        s.append(sNote)
+        #if there are no more available notes, change the chord, rinse and repeat
+        if len(notes) == 0:
+            chords[len(s)-1] = random.choice(chordList) + inversionGenerator()
+            chord = chords[len(s)-1]
+            root = int(chord[0])
+            inversion = chord[1:3]
+            if inversion == "  ":
+                bNote = root
+            elif inversion == "6 ":
+                bNote = root + 2
+            else:
+                bNote = root + 4
+            if bNote >= 8:
+                bNote = bNote - 7
+            bNote = bNote + 7
+            b[len(s) - 1] = (int(bNote))
+    #print(s)				
+    #print(s[len(s)-2:len(s)+1],b[len(s)-2:len(s)])
 
-#print(s)
-#print(b)
+print(s)
+print(a)
+print(t)
+print(b)
 #print(chords)
+#print(beatNotes)
